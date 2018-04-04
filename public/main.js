b2ed533d46a3993ebe5fadbef5b7bb4d38cfd7c3
Sparks <dsparks@proforma.com>
2018-04-03 20:20:56 -0400

    game shows question

--- public/main.js
+++ public/main.js
@@ -1,45 +1,43 @@
 const app = angular.module("geopardyApp", [])
   .controller("mainController", ['$scope', '$http', ($scope, $http) => {
 
-
-    $scope.categories = [
-      {
-        title: "1", question: [{ value: 200, clue: "" }]
-      },
-      {
-        title: "2", question: [{ value: 200, clue: "" }]
-      },
-      {
-        title: "3", question: [{ value: 200, clue: "" }]
-      },
-      {
-        title: "4", question: [{ value: 200, clue: "" }]
-      },
-      {
-        title: "5", question: [{ value: 200, clue: "" }]
-      }
-    ];
-
-
-    $scope.displayQuestion = (value) => {
-      $http({
-        method: "GET",
-        url: "http://jservice.io/api/clues",
-      }).then(response => {
-        console.log("loaded question", response)
-        console.log("loaded data", response.data)
-        $scope.data = response.data;
-        for (let i = 0; i < 5; i++) {
-          $scope.statment = data[i].question;
-          $scope.values = data[i].value;
-          $scope.answers = data[i].answer;
-        }
-      })
-      return categories.question={
-        value: $scope.values,
-        answers: $scope.answers,
-      };
+    const CATEGORY_URL = "http://jservice.io/api/category?id=11547";
+    $scope.player = {
+      money: 0,
     }
 
+      $http({
+        method: "GET",
+        url: CATEGORY_URL,
+      }).then(response => {
+        console.log("loaded data", response.data)
+        $scope.category = {
+          title: response.data.title,
+          clues: response.data.clues.slice(0,5),
+        }
+
+        $scope.category.clues = $scope.category.clues.map((clue, index) =>{
+          clue.value = (index +1) *100;
+          return clue;
+        })
+      })
+
+      $scope.questionClicked=(clue)=>{
+        console.log("question clicked", clue)
+        $scope.questionWasClicked = true;
+        $scope.questionSelected = clue;
+      }
+
+      $scope.checkAnswer=()=>{
+        console.log("here")
+        const isCorrectAnswer = $scope.userAnswer.toLowerCase() === $scope.questionSelected.answer.toLowerCase();
+        if(isCorrectAnswer){
+          $scope.player.score += parseInt($scope.questionSelected.value);
+        }
+        else{
+          $scope.player.score -= parseInt($scope.questionSelected.value);
+        }
+      } 
+    
   }
   ]);
